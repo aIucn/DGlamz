2c7fe88187c230d9379002f204aaf59d8d5dc527
aIucn <ethanblackman194@gmail.com>
2025-12-04 10:00:59 -0400

    Update db.php

--- D'Glamz 473/db.php
+++ D'Glamz 473/db.php
@@ -1,12 +1,11 @@
 <?php
-$servername = "localhost"; 
-$username = "root"; 
-$password = ""; 
+$servername = "mysql-3d81e3-ethanblackman194-caeb.g.aivencloud.com";
+$username = "avnadmin";
+$password = "AVNS_vTB5oWJ0EgsUU0YnIUa";
 $dbname = "mywebsite";
+$port = 21018;
 
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
